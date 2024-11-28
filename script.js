cb83c6a1a3e0296976bcf69c6f0677750739e911
nainchawla <159126766+nainchawla@users.noreply.github.com>
2024-11-29 01:54:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Toggle the side drawer
+function toggleDrawer() {
+  const drawer = document.getElementById("side-drawer");
+  drawer.classList.toggle("open");
+}
+
+// Close the drawer
+function closeDrawer() {
+  const drawer = document.getElementById("side-drawer");
+  drawer.classList.remove("open");
+}
+
+// Show the mess selection menu and close the drawer
+function resetMessType() {
+  closeDrawer(); // Close drawer after clicking reset
+  document.getElementById('mess-selection').classList.remove('hidden');
+  document.getElementById("mess-menu").classList.add("hidden");
+}
+
+function resetMenu() {
+  const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
+  console.log('Reset menu triggered');
+  toggleDrawer(); // Close drawer
+}
+
+function redirectToVeg() {
+  window.location.href = 'veg.html';
+}
+
+function redirectToNonVeg() {
+  window.location.href = 'nonveg.html';
+}
+
+function redirectToSpecial() {
+  window.location.href = 'special.html';
+}
+
+// This function will run once the page is fully loaded
+window.onload = function() {
+  // Remove the 'hidden' class from the mess selection div
+  document.getElementById('mess-selection').classList.remove('hidden');
+  // Ensure the mess menu is hidden when the page loads
+  document.getElementById('mess-menu').classList.add('hidden');
+};
